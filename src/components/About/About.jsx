a379c1b0351003c18b5636d1ee35710242143cec
kunaltijare <kunal.tijare@source.one>
2023-10-19 22:46:14 +0530

    updated about me and blogs Metadata

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,22 +1,35 @@
-import "./about.css"
+import "./about.css";
 
 export const About = () => {
-    return (
-        <div id="about">
-            <div className="faint-heading">&lt; About /&gt;</div>
-            <div className="about">
-                <div className="about-info">
-                    <p style={{ padding: "0.5rem" }}>
-                        Hello! I'm <span className="hey" >Kunal TIjare</span><br />
-                        A Full Stack JavaScript Developer with an eye for UI-UX, I’ve experience in building end-to-end web-apps across MERN stack.
-                        <br />
-                        I love travelling & especially the road trips in monsoon with some good music to listen, i also love playing racing games and finding answers to weird questions.
-                    </p>
-                </div>
-                <div className="hero-img-container">
-                    <img className="about-img" width="100%" height="auto" src="Images/workingCharacter.png" alt="hero" />
-                </div>
-            </div>
+  return (
+    <div id="about">
+      <div className="faint-heading">&lt; About /&gt;</div>
+      <div className="about">
+        <div className="about-info">
+          <p style={{ padding: "0.5rem" }}>
+            Hello! I'm <span className="hey">Kunal Tijare</span>
+            <br />
+            A JavaScript Developer with an eye for UI-UX, I’ve experience in
+            building end-to-end web-apps across MERN stack.I Love building
+            things on web.
+            <br />
+            Currently working as a full stack developer at Source.One <br />
+            <br /> When I'm not immersed in the digital world, you'll find me
+            exploring the great outdoors. Hiking, trekking ⛰️ & especially going
+            for road trips in monsoon. I also enjoy some lazy 🥱 weekends
+            streaming movies and shows.
+          </p>
         </div>
-    )
-}
+        <div className="hero-img-container">
+          <img
+            className="about-img"
+            width="100%"
+            height="auto"
+            src="Images/workingCharacter.png"
+            alt="hero"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/utils.js
+++ src/utils.js
@@ -1,66 +1,74 @@
 export const projectsData = [
-    {
-        id: 1,
-        name: "Social Monks",
-        imgUrl: "https://ik.imagekit.io/abcdefg/ProjectImages/socialmonks_5pJgIVgXBGCw.jpeg?updatedAt=1635149694175",
-        info: "A Social Media app for minimialist's",
-        github: "https://github.com/Dev-kunal/Social-Monks",
-        live: "https://social-monks.netlify.app"
-    },
-    {
-        id: 2,
-        name: "Zen Tech Store",
-        imgUrl: "https://ik.imagekit.io/abcdefg/ProjectImages/techstore_0iUvPeeot.jpeg?updatedAt=1635148380934",
-        info: "An E-Comm app for Tech products",
-        github: "https://github.com/Dev-kunal/EcommerceApp-Zen-TechStore",
-        live: "https://zen-tech-store.netlify.app"
-    },
-    {
-        id: 3,
-        name: "TechTube",
-        imgUrl: "https://ik.imagekit.io/abcdefg/ProjectImages/techtube_KYGzhy_IXsP.jpeg?updatedAt=1635148381939",
-        info: "A Tech Video Library app",
-        github: "https://github.com/Dev-kunal/VideoLibrary-TechTube",
-        live: "https://tech-tube.netlify.app/"
-    },
-    {
-        id: 4,
-        name: "Quizzy",
-        imgUrl: "https://ik.imagekit.io/abcdefg/ProjectImages/quizzy_3g0dS4XCjn.jpeg?updatedAt=1635331501993",
-        info: "A Quiz app build using Typescript",
-        github: "https://github.com/Dev-kunal/Quiz-App",
-        live: "https://zen-quizzy.netlify.app/"
-    },
-    {
-        id: 5,
-        name: "Zen-ui",
-        imgUrl: "https://ik.imagekit.io/abcdefg/ProjectImages/buttons_tMoWax86s.jpeg?updatedAt=1635331715969",
-        info: "A CSS Component Library",
-        github: "https://github.com/Dev-kunal/Components-Library-ZenUi",
-        live: "https://zenui.netlify.app/"
-    }
-]
+  {
+    id: 1,
+    name: "Social Monks",
+    imgUrl:
+      "https://ik.imagekit.io/abcdefg/ProjectImages/socialmonks_5pJgIVgXBGCw.jpeg?updatedAt=1635149694175",
+    info: "A Social Media app for minimialist's",
+    github: "https://github.com/Dev-kunal/Social-Monks",
+    live: "https://social-monks.netlify.app",
+  },
+  {
+    id: 2,
+    name: "Zen Tech Store",
+    imgUrl:
+      "https://ik.imagekit.io/abcdefg/ProjectImages/techstore_0iUvPeeot.jpeg?updatedAt=1635148380934",
+    info: "An E-Comm app for Tech products",
+    github: "https://github.com/Dev-kunal/EcommerceApp-Zen-TechStore",
+    live: "https://zen-tech-store.netlify.app",
+  },
+  {
+    id: 3,
+    name: "TechTube",
+    imgUrl:
+      "https://ik.imagekit.io/abcdefg/ProjectImages/techtube_KYGzhy_IXsP.jpeg?updatedAt=1635148381939",
+    info: "A Tech Video Library app",
+    github: "https://github.com/Dev-kunal/VideoLibrary-TechTube",
+    live: "https://tech-tube.netlify.app/",
+  },
+  {
+    id: 4,
+    name: "Quizzy",
+    imgUrl:
+      "https://ik.imagekit.io/abcdefg/ProjectImages/quizzy_3g0dS4XCjn.jpeg?updatedAt=1635331501993",
+    info: "A Quiz app build using Typescript",
+    github: "https://github.com/Dev-kunal/Quiz-App",
+    live: "https://zen-quizzy.netlify.app/",
+  },
+  {
+    id: 5,
+    name: "Zen-ui",
+    imgUrl:
+      "https://ik.imagekit.io/abcdefg/ProjectImages/buttons_tMoWax86s.jpeg?updatedAt=1635331715969",
+    info: "A CSS Component Library",
+    github: "https://github.com/Dev-kunal/Components-Library-ZenUi",
+    live: "https://zenui.netlify.app/",
+  },
+];
 
 export const blogsData = [
-    {
-        id: 1,
-        headerUrl: "https://kunal-tijare.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1626585311370%2Fu4HhHzhgvk.png%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=3840&q=75",
-        title: "How JavaScipt works and executes your program under the hood",
-        date: "Aug 5, 2021",
-        url: "https://kunal-tijare.hashnode.dev/how-javascipt-works-and-executes-your-program-under-the-hood"
-    },
-    {
-        id: 2,
-        headerUrl: "https://kunal-tijare.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1626612589184%2F21mAd6Wiz.png%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=3840&q=75",
-        title: "Scope, Lexical Environment, and Scope Chain in JavaScript",
-        date: "Aug 9, 2021",
-        url: "https://kunal-tijare.hashnode.dev/scope-lexical-environment-and-scope-chain-in-javascript"
-    },
-    {
-        id: 3,
-        headerUrl: "https://kunal-tijare.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1626597979516%2FYI3YWAp7v.png%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=3840&q=75",
-        title: "Hoisting in JavaScript, ( in the easiest way )",
-        date: "Aug 10, 2021",
-        url: "https://kunal-tijare.hashnode.dev/hoisting-in-javascript"
-    }
-]
+  {
+    id: 1,
+    headerUrl:
+      "https://cdn.hashnode.com/res/hashnode/image/upload/v1626585311370/u4HhHzhgvk.png?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
+    title: "How JavaScipt works and executes your program under the hood",
+    date: "Aug 5, 2021",
+    url: "https://kunal-tijare.hashnode.dev/how-javascipt-works-and-executes-your-program-under-the-hood",
+  },
+  {
+    id: 2,
+    headerUrl:
+      "https://cdn.hashnode.com/res/hashnode/image/upload/v1626612589184/21mAd6Wiz.png?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
+    title: "Scope, Lexical Environment, and Scope Chain in JavaScript",
+    date: "Aug 9, 2021",
+    url: "https://kunal-tijare.hashnode.dev/scope-lexical-environment-and-scope-chain-in-javascript",
+  },
+  {
+    id: 3,
+    headerUrl:
+      "https://cdn.hashnode.com/res/hashnode/image/upload/v1626597979516/YI3YWAp7v.png?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
+    title: "Hoisting in JavaScript, ( in the easiest way )",
+    date: "Aug 10, 2021",
+    url: "https://kunal-tijare.hashnode.dev/hoisting-in-javascript-in-the-easiest-way",
+  },
+];
